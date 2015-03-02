884af6326c45277364726358e87f7ea352a75c01
Andrew Dyer <abdyer@gmail.com>
2015-03-02 17:58:14 -0600

    Finish initial development of plugin. Coordinate setting version name/code, building the specified variant, and uploading the APK & release notes (if present) to Amazon S3.

--- android/gradle-plugin/plugin/src/main/groovy/com/chaione/build/gradle/ChaiOneBuildPlugin.groovy
+++ android/gradle-plugin/plugin/src/main/groovy/com/chaione/build/gradle/ChaiOneBuildPlugin.groovy
@@ -1,18 +1,77 @@
 package com.chaione.build.gradle;
 
-import org.gradle.api.*;
+import org.gradle.api.*
 
 class ChaiOneBuildPlugin implements Plugin {
+
+  Project project
+
   def void apply(Object project) {
-    project.task('internalAppStoreDeploy') << {
-      def sha1 = execShellCommand(project, 'git', ['rev-parse', '--short', 'HEAD'])
-      def branch = execShellCommand(project, 'git', ['rev-parse', '--abbrev-ref', 'HEAD'])
-      println "ChaiOne Build Plugin activate! ${sha1}_${branch}"
+    this.project = project
+
+    // Get version code first otherwise it returns -1 for some reason
+    def versionCode = getVersionCode()
+
+    project.android.applicationVariants.all { variant ->
+      project.task("internalAppStoreDeploy${variant.name.capitalize()}", description: "Deploys a ${variant.name} build to the ChaiOne internal app store.") << {
+        def versionName = getVersionName()
+        println "\nChaiOne Gradle build plugin activate!\n   Version Name: ${versionName}\n   Version Code: ${versionCode}"
+
+        def config = project.android.defaultConfig
+        config.versionName = versionName
+        config.versionCode = versionCode
+
+        performBuild(variant.name)
+
+        uploadApk(variant)
+        uploadReleaseNotes(variant)
+      }
     }
   }
 
-  def execShellCommand(project, command, arguments) {
+  def getVersionName() {
+    def currentName = project.android.defaultConfig.versionName
+    def branch = execShellCommand('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
+
+    if(branch == "master" || branch == "dev") {
+      currentName
+    }
+    else {
+      "${currentName}-${branch}".toString()
+    }
+  }
+
+  def getVersionCode() {
+    def result = Integer.parseInt(execShellCommand('git', ['rev-list', '--count', 'HEAD']))
+    result > 0 ? result : 1
+  }
+
+  def performBuild(variantName) {
+    println "\nPerforming build...\n"
+    println execShellCommand('gradle', ["assemble${variantName.capitalize()}"])
+  }
+
+  def uploadApk(variant) {
+    println '\nUploading APK to S3...\n'
+    println execShellCommand('s3cmd', ['put', '--recursive', '--exclude', '*-unaligned.apk', '--exclude', '*.txt', "${project.getBuildDir()}/outputs/apk/", 's3://chaione-app-store/_inbox/'])
+  }
+
+  def uploadReleaseNotes(variant) {
+    variant.outputs.each { output ->
+      def outputFile = output.outputFile
+      if (outputFile != null && outputFile.name.endsWith('.apk') && !outputFile.name.contains('-unaligned')) {
+        def releaseNotesFileName = outputFile.name.replace('.apk', '_RELEASE_NOTES.md')
+        if(project.file(releaseNotesFileName).exists()) {
+          println '\nUploading release notes to S3...\n'
+          println execShellCommand('s3cmd', ['put', "${project.getProjectDir()}/${releaseNotesFileName}", "s3://chaione-app-store/_inbox/${releaseNotesFileName}"])
+        }
+      }
+    }
+  }
+
+  def execShellCommand(command, arguments) {
     def stdout = new ByteArrayOutputStream()
+
     project.exec {
       executable command
       args arguments
